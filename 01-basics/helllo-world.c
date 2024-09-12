a608dc43de928e4ec6f098e0fcf25d947fb257b5
SunilOS <sunilos007@gmail.com>
2024-09-12 15:05:22 +0530

    naming convention corrected

--- 01-basics/helllo-world.c
+++ 01-basics/helllo-world.c
@@ -1,6 +1,7 @@
 #include <stdio.h>  // Includes the standard input-output library for input-output functions
 
 int main() {  // Main function where the program execution starts
+
     // Prints "Hello, World!" followed by a newline character to the console
     printf("Hello, World!\n");  // %s is not required here because it's a direct string literal
 
