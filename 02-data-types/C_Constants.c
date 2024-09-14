0143083f1f5452b3645d06922b42d74a1e9ab50b
ani9977 <105789443+ani9977@users.noreply.github.com>
2024-09-14 12:56:03 +0530

    Done

--- 02-data-types/C_Constants.c
+++ 02-data-types/C_Constants.c
@@ -1,9 +1,8 @@
+#include <stdio.h>
 
-#include <stdio.h>  // Includes the standard input-output library
+#define PI 3.14159
 
-#define PI 3.14159  // Defines a constant PI with the value 3.14159
-
-int main() {  // Main function where the program starts executing
-    printf("PI value: %f\n", PI);  // Prints the value of PI using the %f format specifier for floating-point numbers
-    return 0;  // Returns 0, indicating that the program has run successfully
+int main() {
+    printf("PI value: %f\n", PI);
+    return 0;
 }
