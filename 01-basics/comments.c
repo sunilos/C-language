4951dc7fa4b09f10a14345039f1abb53bbd1229b
ani9977 <105789443+ani9977@users.noreply.github.com>
2024-09-14 12:15:43 +0530

    Update comments.c

--- 01-basics/comments.c
+++ 01-basics/comments.c
@@ -1,16 +1,6 @@
-#include <stdio.h>  // Preprocessor directive to include the standard input-output library
+#include <stdio.h>
 
-/*
-* This program shows how to define single and multiline comments in a C program -- multiline comment 
-*/
-int main() {  // Main function where program execution begins  --Single line comment
-    
-    // This is a single-line comment explaining that the next line prints a message.
-    
-    /* This is a multi-line comment.
-       Multi-line comments allow you to add detailed descriptions or
-       explanations across several lines. 
-    */
-    printf("Comments in C.\n");  // Prints the message "Comments in C." followed by a new line
-    return 0;  // Returns 0 indicating that the program executed successfully
+int main() {
+    printf("Comments in C.\n");
+    return 0;
 }
