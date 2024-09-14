e24daf8a8fdd6c97921d8241d2b40dc0ec6a53f4
ani9977 <105789443+ani9977@users.noreply.github.com>
2024-09-14 13:02:16 +0530

    Done

--- 02-data-types/C_Variables.c
+++ 02-data-types/C_Variables.c
@@ -1,17 +1,7 @@
 #include <stdio.h>
 
-/**
- * @brief The main function demonstrates basic variable usage and printing in C.
- * 
- * This function initializes an integer variable, assigns it a value, and prints that value.
- * 
- * @return int Returns 0 to indicate successful execution.
- */
 int main() {
-    int myVar = 5;  /**< Integer variable initialized with the value 5. */
-
-    // Print the value of the integer variable
+    int myVar = 5;
     printf("Variable value: %d\n", myVar);
-
-    return 0;   /**< Return 0 to indicate successful execution. */
+    return 0;
 }
