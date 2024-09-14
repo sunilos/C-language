426c472ed02b2c1e21f9ba86f9d2bc8ef27fecec
ani9977 <105789443+ani9977@users.noreply.github.com>
2024-09-14 12:25:29 +0530

    Done

--- 01-basics/output.c
+++ 01-basics/output.c
@@ -1,8 +1,6 @@
-#include <stdio.h>  // Includes the standard input-output library for input-output operations
+#include <stdio.h>
 
-int main() {  // Main function where the program execution begins
-    // Prints the message "This is an example of output in C." to the console
-    printf("This is an example of output in C.\n");  // %s is not needed as it's a direct string literal
-
-    return 0;  // Returns 0, indicating successful program execution
+int main() {
+    printf("This is an example of output in C.\n");
+    return 0;
 }
